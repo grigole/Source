104ae877018c0330c4ae6700fcc86d39b91adf6c
Greg Rigole <grigole@gvrsoft.com>
2018-02-11 11:56:52 -0500

    Added Eclipse project Math

---
+++ eclipse/Math/src/com/gvrsoft/demo/math/mathDemo.java
@@ -0,0 +1,13 @@
+package com.gvrsoft.demo.math;
+
+public class mathDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow World!");
+
+		System.out.printf( "PI is %f%n", Math.PI );
+		System.out.printf( "PI is %.15f%n", Math.PI );
+	}
+
+}
