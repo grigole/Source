ad0cccf8caa5e51e5be86a493f706ab23852773b
Greg Rigole <grigole@gvrsoft.com>
2019-09-17 15:27:40 -0400

    Misc updates

---
+++ eclipse/Math/src/com/gvrsoft/demo/math/MathTests.java
@@ -0,0 +1,13 @@
+package com.gvrsoft.demo.math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({ mathDemoTest.class })
+
+public class MathTests {
+
+}
