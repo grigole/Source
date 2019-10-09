ec07640d95ce0560e823cdb5862eb18fe92213a4
Greg Rigole <grigole@gvrsoft.com>
2019-10-08 22:10:07 -0400

    Added log4c to project.
    Added log files to .gitignore

--- eclipse/TCPEcho/client.cpp
+++ eclipse/TCPEcho/client.cpp
@@ -5,7 +5,18 @@
  *      Author: grigole
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <log4c.h>
+
+log4c_category_t* mycat = NULL;
+
 int main( int argc, char **argv )
 {
+	log4c_init();
+	mycat = log4c_category_get( "TCPEcho.log" );
+
+	log4c_category_log( mycat, LOG4C_PRIORITY_DEBUG, "argc = %d", argc );
+
 	return 0;
 }
