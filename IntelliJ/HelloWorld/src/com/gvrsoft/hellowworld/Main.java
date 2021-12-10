f23c8a68a9f11befbb14ecb56f89daf562e08a9f
Greg Rigole <grigole@gvrsoft.com>
2021-12-09 20:11:24 -0500

    Fixed HelloWorld

---
+++ IntelliJ/HelloWorld/src/com/gvrsoft/hellowworld/Main.java
@@ -0,0 +1,10 @@
+package com.gvrsoft.hellowworld;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    out.println( "Hello World!");
+    }
+}
