2929d7b756e5b3bf50668fd8e2b947d451e4e86e
Greg Rigole <grigole@gvrsoft.com>
2023-05-18 16:27:07 -0400

    Examples of QString and QMap

--- qt/trials/main.cpp
+++ qt/trials/main.cpp
@@ -1,13 +1,24 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QString>
+#include <QMap>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QString str = "Greg Rigole";
+    QMap<QString,int> map;
 
     qInfo() << "Testing QInfo" << str;
+
+    map.insert( "Greg", 56 );
+    map.insert( "Marlene", 81 );
+    map.insert( "Victor", 86 );
+    map.insert( "Shelley", 54 );
+    map.insert( "Roberta", 51 );
+
+    qInfo() << map;
+    qInfo() << "Greg is" << map["Greg"];
 
     return a.exec();
 }
